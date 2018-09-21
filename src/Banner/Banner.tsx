9a60b887d8e30f1da26122c107c23a6a023d664c
Lucia Velasco <luciakvelasco@gmail.com>
2018-09-21 10:11:35 +0100

    New images, copy and styling

--- src/Banner/Banner.tsx
+++ src/Banner/Banner.tsx
@@ -7,12 +7,15 @@ export class Banner extends React.Component {
   public render() {
     return (
       <div className="App-header">
-        <div className="logo-block">
-          <h1 className="logo-title">Calima</h1>
-        </div>
-        <div className="intro-title">
-          <p>Lucia Velasco</p>
-          <p>Process Optimisation and Management Consultancy</p>
+        <div className="header-content">
+          <div className="logo-block">
+            <h1 className="logo-title">Calima</h1>
+          </div>
+          <div className="intro-title">
+            {/*<p className="my-name">LUCIA VELASCO</p>*/}
+            <p>PROCESS OPTIMISATION AND MANAGEMENT CONSULTANCY</p>
+            {/*<p>Process Optimisation and Management Consultancy</p>*/}
+          </div>
         </div>
         <CtaSolid className={[ 'cta-top' ]} text="Get in touch for..."/>
       </div>
--- src/MethodAndContact/MethodAndContact.tsx
+++ src/MethodAndContact/MethodAndContact.tsx
@@ -8,13 +8,18 @@ export class MethodAndContact extends React.Component {
     return (
       <div className="method-and-contact">
         <div className="contact">
-          <h4 className="contact-title">Calima</h4>
-          <p>Lucia Velasco</p>
-          <a className="cta-button" href="mailto:luciakvelasco@gmail.com">Get in touch if...</a>
+          <h4 className="contact-title">Lucia Velasco</h4>
+          <p>Call me: 07887393021</p>
+          <p>Email me: <a href="mailto:luciakvelasco@gmail.com">luciakvelasco@gmail.com</a></p>
+          {/*<p>Connect: <a href="mailto:luciakvelasco@gmail.com">LinkedIn/Skype/Facebook(?)/</a></p>*/}
         </div>
         <div className="method">
           <h5 className="method-title">Calima</h5>
           <p>Process Optimisation and Management Consultancy</p>
+          <p>Lucia worked in technology for several years, as a software developer
+            and as a Scrum Master, where she touched on healthcare, finance,
+            travel and energy sectors, among others.
+            In that time she has championed Agile Processes and best practice to help her employers grow.</p>
         </div>
         <CtaFlat text="Get in touch!"/>
       </div>
